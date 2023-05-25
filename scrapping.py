9cf154bdeabdacb52b6f77efe76d749661d8cde1
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-05-25 12:43:37 -0500

    Getting attractions

--- scrapping.py
+++ scrapping.py
@@ -1,12 +1,11 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
-import requests
-from bs4 import BeautifulSoup
-from lxml import etree
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9,es;q=0.8"
-}
+CHROME_DRIVER_PATH = '/Users/segc/Documents/chromedriver/chromedriver'
 
 
 class Attraction:
@@ -18,15 +17,11 @@ attractions: list[Attraction] = []
 
 
 class TripAdvisorScrapper:
-    url = 'https://www.tripadvisor.co/Attraction_Review-g294074-d532215-Reviews-Museo_Botero_del_Banco_de_la_Republica-Bogota.html'
+    url = 'https://www.tripadvisor.co/Attraction_Review-g294074-d550644-Reviews-Barrio_La_Candelaria-Bogota.html'
 
-    def __init__(self) -> None:
-        self.page = requests.get(self.url, headers=headers)
-        self.soup: BeautifulSoup = BeautifulSoup(
-            self.page.content, 'html.parser'
-        )
-        print(self.soup)
-        self.dom = etree.HTML(str(self.soup))
+    def __init__(self, path) -> None:
+        self.driver: Chrome = webdriver.Chrome()
+        self.driver.get(self.url)
 
     def get_site_description(self):
         try:
@@ -41,22 +36,39 @@ class TripAdvisorScrapper:
                 print(e)
 
     def get_site_attractions_strategy_1(self):
-        title = self.dom.xpath(
-            '/html/body/div[1]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1')
-        print(title[0].text)
-        body = self.dom.xpath(
-            '/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div')
-        print(body[0].text)
+        title = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div[1]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1'
+        )
+        print(title.text)
+        duration = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[3]/div[2]'
+        )
+        print(duration.text)
+        body = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div'
+        )
+        print(body.text)
 
     def get_site_attractions_strategy_2(self):
-        title = self.dom.xpath(
-            '/html/body/div[1]')
-        print(title)
-        # body = self.dom.xpath(
-        #    '/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div')
-        # print(body[0].text)
+        title = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div[1]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1'
+        )
+        print(title.text)
+        duration = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[3]/div[2]'
+        )
+        print(duration.text)
+        body = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div')
+        print(body.text)
 
 
-scrapper = TripAdvisorScrapper()
+scrapper = TripAdvisorScrapper(CHROME_DRIVER_PATH)
 time.sleep(1)
 scrapper.get_site_description()
